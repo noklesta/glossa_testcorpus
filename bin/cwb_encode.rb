0b5bf7b972b142544ffc4d1f6802a3a8a1a52e62
Anders NÃ¸klestad <anders.noklestad@iln.uio.no>
2010-04-20 20:45:36 +0200

    Use FileUtils.rm

--- bin/cwb_encode.rb
+++ bin/cwb_encode.rb
@@ -7,7 +7,7 @@ DATA_DIR = 'corpus_data'
 CORPUS_NAME = 'TEST'
 
 # Remove old data files (necessary to avoid weird errors!)
-system("rm #{DATA_DIR}/#{CORPUS_NAME}/*")
+FileUtils.rm("#{DATA_DIR}/#{CORPUS_NAME}/*", :force => true)
 
 # Encode the corpus data
 system("#{CWB_BINARY_PATH}/cwb-encode -xsB -f #{TAB_FILE_NAME} -d #{DATA_DIR}/#{CORPUS_NAME} -s -D -P lemma -P pos")
