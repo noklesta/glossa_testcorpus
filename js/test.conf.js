7b40b2b4c47f9b8760e488d1aafe988282512599
Anders NÃ¸klestad <anders.noklestad@iln.uio.no>
2010-04-21 18:13:17 +0200

    Added conf.js file

---
+++ js/test.conf.js
@@ -0,0 +1,14 @@
+
+var languageOpts = new Array;
+
+languageOpts = [['TEST', 'English','en']];
+
+var htmlRoot = 'http://www.example.org/glossa/';
+var cgiRoot = 'http://www.example.org/cgi-bin/glossa/';
+
+var conf = new Array;
+
+conf['type'] = 'monolingual';
+conf['title'] = 'Search Test Corpus';
+conf['corpus_name'] = 'Test Corpus';
+var language='en';
